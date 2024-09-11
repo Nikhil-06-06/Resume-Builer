e44bbba9dcae1868e1afb6eb014ac8feecca3f2b
Nikhil-06-06 <nikhilraw2@gmail.com>
2024-09-11 18:22:56 +0530

    Updated packages and fixed indentation

--- src/components/presentation/gettingStarted.js
+++ src/components/presentation/gettingStarted.js
@@ -1,68 +1,70 @@
-import React from 'react';
-import {skinCodes} from '../../constants/typeCodes';
-import * as documentAction from '../../redux/actions/actions';
-// import { bindActionCreators } from 'redux';
-// import { withRouter } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { setDocument,updateDocument } from '../../redux/actions/documentActions';
-import {connect} from 'react-redux';
+import React from "react";
+import { skinCodes } from "../../constants/typeCodes";
+import * as documentAction from "../../redux/actions/actions";
+import { useNavigate } from "react-router-dom";
+import {
+  setDocument,
+  updateDocument,
+} from "../../redux/actions/documentActions";
+import { connect } from "react-redux";
 
 function GettingStarted(props) {
+  let navigate = useNavigate();
 
-     let history = useHistory();
-
-     const onChange = async (skinCd) => {
-
-        if(props.document.id!==null){
-             props.updateDocument(skinCd);        
-        }
-        else{
-             props.setDocument(skinCd); 
-        }
-
-        history.push('/contact');
-      }
-
-      
-        return (  
-            <div className="container med gettingStarted">
-                <div className="section">
-                    <h1 className=" center">
-                    Select a resume template to get started</h1>
-                    <p className=" center">
-                    You will be able to edit and change this template later!
-                    </p>
-                    <div className="styleTemplate ">
-                    {
-                        skinCodes.map((value,index) => {
-                            return( <div key={index} className="template-card rounded-border">
-                                        <i className={(value == 'demo-value'? 'selected fa fa-check' :'hide') } ></i>
-                                        <img  className='' src={'/images/' + value + '.svg'}/>
-                                        <button type="button" onClick={()=>onChange(value)}  className='btn-select-theme'>USE TEMPLATE</button>
-                                    </div>);
-    
-                        })
-                    }
-                    </div>
-                
-                </div>
-            </div>
-        );
-    
-}
-  
-const mapStateToProps=(state)=>{
-    return{
-        document: state.document
+  const onChange = async (skinCd) => {
+    if (props.document.id !== null) {
+      props.updateDocument(skinCd);
+    } else {
+      props.setDocument(skinCd);
     }
+
+    navigate("/contact");
+  };
+
+  return (
+    <div className="container med gettingStarted">
+      <div className="section">
+        <h1 className=" center">Select a resume template to get started</h1>
+        <p className=" center">
+          You will be able to edit and change this template later!
+        </p>
+        <div className="styleTemplate ">
+          {skinCodes.map((value, index) => {
+            return (
+              <div key={index} className="template-card rounded-border">
+                <i
+                  className={
+                    value == "demo-value" ? "selected fa fa-check" : "hide"
+                  }
+                ></i>
+                <img className="" src={"/images/" + value + ".svg"} />
+                <button
+                  type="button"
+                  onClick={() => onChange(value)}
+                  className="btn-select-theme"
+                >
+                  USE TEMPLATE
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        setDocument : (skincd)=>dispatch(setDocument(skincd)),
-        updateDocument : (skincd)=>dispatch(updateDocument(skincd))
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    document: state.document,
+  };
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(GettingStarted);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setDocument: (skincd) => dispatch(setDocument(skincd)),
+    updateDocument: (skincd) => dispatch(updateDocument(skincd)),
+  };
+};
 
+export default connect(mapStateToProps, mapDispatchToProps)(GettingStarted);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'; 
-import { legacy_createStore as createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import rootReducer from './redux/reducers/rootReducer';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import thunk from 'redux-thunk';
-import {reduxFirestore, getFirestore} from 'redux-firestore';
-import {getFirebase,ReactReduxFirebaseProvider} from 'react-redux-firebase';
-import {createFirestoreInstance} from 'redux-firestore';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./redux/reducers/rootReducer";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { configureStore } from '@reduxjs/toolkit';
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { getFirebase, ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-0KUzn7kH7AGC924YEkWvk2uLn24Ozyk",
@@ -20,29 +20,46 @@ const firebaseConfig = {
   projectId: "resume-builder-4e9f1",
   storageBucket: "resume-builder-4e9f1.appspot.com",
   messagingSenderId: "436699531204",
-  appId: "1:436699531204:web:111b6de547f2b091cb0691"
+  appId: "1:436699531204:web:111b6de547f2b091cb0691",
 };
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
 
-const reduxStore=createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),reduxFirestore(firebase)));
+// console.log('here', typeof thunk);
+
+// const reduxStore = createStore(
+//   rootReducer,
+//   composeWithDevTools(
+//     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+//     reduxFirestore(firebase)
+//   )
+// );
+
+const reduxStore = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: { getFirebase, getFirestore }
+      }
+    }),
+  reduxFirestore: firebase
+})
 
 ReactDOM.render(
-
-    <BrowserRouter>
-      <Provider store={reduxStore}>
-        <ReactReduxFirebaseProvider
+  <BrowserRouter>
+    <Provider store={reduxStore}>
+      <ReactReduxFirebaseProvider
         firebase={firebase}
         config={firebaseConfig}
         dispatch={reduxStore.dispatch}
-        createFirestoreInstance={createFirestoreInstance}>
-          <App />
-        </ReactReduxFirebaseProvider>
-      </Provider>
-    </BrowserRouter>
-  
-    
-,
-  document.getElementById('root')
+        createFirestoreInstance={createFirestoreInstance}
+      >
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
+  </BrowserRouter>,
+
+  document.getElementById("root")
 );
